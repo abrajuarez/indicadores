a01c4ddf5f5de95c9e870f638aa66529bc58958b
kozmoz3 <kiqueyo847@gmail.com>
2020-01-10 21:29:27 -0600

    test1

--- src/main/java/com/indicador/mapfre/IndicadormapfreApplication.java
+++ src/main/java/com/indicador/mapfre/IndicadormapfreApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class IndicadormapfreApplication extends SpringBootServletInitializer{
 	
-	
 	@Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(IndicadormapfreApplication.class);
